aa36e30778f3bd1f63e22d3df273a13b6720c6bd
Bruno Rodrigues <bruno.rodrigues201800@gmail.com>
2025-01-17 21:23:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var ntxt = document.querySelector('input#numero')
+var btn = document.querySelector('button#verificar')
+var info = document.querySelector('div#informacao')
+var resu = document.querySelector('div#res')
+
+btn.addEventListener('click', verificar)
+resu.style.display = 'none'
+
+function verificar() {
+    info.innerHTML = 'informação'
+    var nvalor = Number(ntxt.value)
+
+    if (nvalor <= 0) {
+        resu.style.display = 'none'
+        info.innerHTML = 'Número inválido'
+        alert('[ERROR] Números inferiores ou iguais a zero (0) são inválidos')
+    } else {
+        resu.style.display = 'block'
+    }
+
+    if (nvalor % 2 === 0) {
+        resu.innerHTML = `O número ${nvalor} é <strong>PAR<strong>`
+    } else {
+        resu.innerHTML = `O número ${nvalor} é <strong>IMPAR<strong>`
+    }
+
+}
